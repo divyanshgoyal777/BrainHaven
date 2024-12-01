ad1f521c468283f0976d17c98777f5a2a2b292d8
Animesh-Prakash <animeshp1607@gmail.com>
2024-12-01 20:53:30 +0530

    update

--- frontend/src/components/Roadmaps/Roadmaps.jsx
+++ frontend/src/components/Roadmaps/Roadmaps.jsx
@@ -12,7 +12,7 @@ const Roadmaps = () => {
         <h1 className="bg-gradient-to-tl from-indigo-600 to-purple-600 bg-clip-text text-transparent text-4xl md:text-5xl font-bold text-center sm:text-4xl">Discover Your Path in Tech</h1>
         <div className="flex flex-col items-center md:gap-8 py-20 w-[75%] lg:w-[60%] m-auto ">
           <div className="flex flex-col items-center">
-            <h1 className="text-2xl sm:text-3xl font-bold text-white text-center w-[80vw] md:w-[60vw] py-4 px-3 sm:px-8  rounded-lg shadow-xl bg-gradient-to-r from-indigo-600 to-purple-600 transform hover:scale-105 transition duration-300 ease-in-out sm:text-2xl">Your Roadmap to Success</h1>
+            <h1 className="text-2xl sm:text-3xl font-bold text-white text-center w-[80vw] md:w-[60vw] py-4 px-3 sm:px-8  rounded-lg shadow-xl bg-gradient-to-r from-indigo-600 to-purple-600 transform hover:scale-105 transition duration-300 ease-in-out ">Your Roadmap to Success</h1>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 mt-8 px-4">
               <button className="px-6 py-3 bg-gradient-to-r from-gray-700 to-gray-900 hover:from-gray-900 hover:to-gray-700 text-white font-semibold rounded-lg shadow-lg transition duration-300 ease-in-out">Frontend Web Development</button>
               <button className="px-6 py-3 bg-gradient-to-r from-gray-700 to-gray-900 hover:from-gray-900 hover:to-gray-700 text-white font-semibold rounded-lg shadow-lg transition duration-300 ease-in-out">Backend Web Development</button>
@@ -23,7 +23,7 @@ const Roadmaps = () => {
             </div>
           </div>
           <div className="my-24 flex flex-col items-center">
-          <h1 className=" text-2xl sm:text-3xl font-bold text-white text-center py-4 px-8 w-[80vw] sm:w-[60vw] rounded-lg shadow-xl bg-gradient-to-r from-indigo-600 to-purple-600 transform hover:scale-105 transition duration-300 ease-in-out sm:text-2xl">Programming Languages</h1>
+          <h1 className=" text-2xl sm:text-3xl font-bold text-white text-center py-4 px-8 w-[80vw] sm:w-[60vw] rounded-lg shadow-xl bg-gradient-to-r from-indigo-600 to-purple-600 transform hover:scale-105 transition duration-300 ease-in-out ">Programming Languages</h1>
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 mt-8 px-4">
               <button className="px-6 py-3  bg-gradient-to-r from-gray-700 to-gray-900 hover:from-gray-900 hover:to-gray-700 text-white font-semibold rounded-lg shadow-lg transition duration-300 ease-in-out">Python</button>
               <button className="px-6 py-3  bg-gradient-to-r from-gray-700 to-gray-900 hover:from-gray-900 hover:to-gray-700 text-white font-semibold rounded-lg shadow-lg transition duration-300 ease-in-out">JavaScript</button>
