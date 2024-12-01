d6190ca24268cf71cbef0976bce36e27bd029185
Animesh-Prakash <animeshp1607@gmail.com>
2024-12-01 20:47:42 +0530

    update

--- frontend/src/components/Resources/Resources.jsx
+++ frontend/src/components/Resources/Resources.jsx
@@ -1,11 +1,50 @@
-import React from "react";
+import { React, useState } from "react";
 import Navbar from "../layout/Navbar/Navbar";
+import Footer from "../layout/Footer/Footer";
 
 const Resources = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const resources = [
+    "Diploma",
+    "B-Tech",
+    "Web Developing",
+    "Software Developing",
+  ];
+
+  const filteredResources = resources.filter((resources) =>
+    resources.toLowerCase().includes(searchTerm.toLowerCase())
+  );
   return (
     <div>
       <Navbar />
-      Resources
+      <div className="mt-40">
+        <h1 className="bg-gradient-to-tl from-indigo-600 to-purple-600 bg-clip-text text-transparent text-4xl md:text-5xl font-bold text-center sm:text-4xl">Notes</h1>
+        <div className="my-16 w-[80%] m-auto flex flex-col items-center">
+          <div className="">
+            <form action="" onSubmit={(e) => e.preventDefault()} className="flex gap-6">
+              <input type="text" placeholder="Search" name="" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} className="w-[60vw] py-3 px-3 rounded-md" />
+              <button className="bg-gradient-to-r from-indigo-600 to-purple-600 text-white font-medium py-2 px-4 rounded-md shadow-md" type="submit">Search</button>
+            </form>
+          </div>
+          <div className="grid grid-cols-2 grid-rows-2 my-20 gap-10">
+            {filteredResources.length > 0 ? (
+              filteredResources.map((resource, index) => (
+                <button
+                  key={index}
+                  className="px-6 py-3 bg-gradient-to-r from-gray-700 to-gray-900 hover:from-gray-900 hover:to-gray-700 text-white font-semibold rounded-lg shadow-lg transition duration-300 ease-in-out"
+                >
+                  {resource}
+                </button>
+              ))
+            ) : (
+              <p className="text-gray-500 text-center col-span-2">
+                No resources found.
+              </p>
+            )}
+          </div>
+        </div>
+      </div>
+      <Footer/>
     </div>
   );
 };
--- frontend/src/components/Roadmaps/Roadmaps.jsx
+++ frontend/src/components/Roadmaps/Roadmaps.jsx
@@ -1,11 +1,47 @@
-import React from "react";
+import {React, useEffect} from "react";
 import Navbar from "../layout/Navbar/Navbar";
-
+import Footer from "../layout/Footer/Footer";
 const Roadmaps = () => {
+  useEffect(()=>{
+    document.title="BrainWave-Roadmaps";
+  },[]);
   return (
     <div>
-      <Navbar />
-      Roadmaps
+      <Navbar/>
+      <div className="mt-32 px-8">
+        <h1 className="bg-gradient-to-tl from-indigo-600 to-purple-600 bg-clip-text text-transparent text-4xl md:text-5xl font-bold text-center sm:text-4xl">Discover Your Path in Tech</h1>
+        <div className="flex flex-col items-center md:gap-8 py-20 w-[75%] lg:w-[60%] m-auto ">
+          <div className="flex flex-col items-center">
+            <h1 className="text-2xl sm:text-3xl font-bold text-white text-center w-[80vw] md:w-[60vw] py-4 px-3 sm:px-8  rounded-lg shadow-xl bg-gradient-to-r from-indigo-600 to-purple-600 transform hover:scale-105 transition duration-300 ease-in-out sm:text-2xl">Your Roadmap to Success</h1>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 mt-8 px-4">
+              <button className="px-6 py-3 bg-gradient-to-r from-gray-700 to-gray-900 hover:from-gray-900 hover:to-gray-700 text-white font-semibold rounded-lg shadow-lg transition duration-300 ease-in-out">Frontend Web Development</button>
+              <button className="px-6 py-3 bg-gradient-to-r from-gray-700 to-gray-900 hover:from-gray-900 hover:to-gray-700 text-white font-semibold rounded-lg shadow-lg transition duration-300 ease-in-out">Backend Web Development</button>
+              <button className="px-6 py-3 bg-gradient-to-r from-gray-700 to-gray-900 hover:from-gray-900 hover:to-gray-700 text-white font-semibold rounded-lg shadow-lg transition duration-300 ease-in-out">Full Stack Development</button>
+              <button className="px-6 py-3 bg-gradient-to-r from-gray-700 to-gray-900 hover:from-gray-900 hover:to-gray-700 text-white font-semibold rounded-lg shadow-lg transition duration-300 ease-in-out ">Data Science</button>
+              <button className="px-6 py-3 bg-gradient-to-r from-gray-700 to-gray-900 hover:from-gray-900 hover:to-gray-700 text-white font-semibold rounded-lg shadow-lg transition duration-300 ease-in-out">BLock Chain</button>
+              <button className="px-6 py-3 bg-gradient-to-r from-gray-700 to-gray-900 hover:from-gray-900 hover:to-gray-700 text-white font-semibold rounded-lg shadow-lg transition duration-300 ease-in-out ">AI and ML</button>
+            </div>
+          </div>
+          <div className="my-24 flex flex-col items-center">
+          <h1 className=" text-2xl sm:text-3xl font-bold text-white text-center py-4 px-8 w-[80vw] sm:w-[60vw] rounded-lg shadow-xl bg-gradient-to-r from-indigo-600 to-purple-600 transform hover:scale-105 transition duration-300 ease-in-out sm:text-2xl">Programming Languages</h1>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 mt-8 px-4">
+              <button className="px-6 py-3  bg-gradient-to-r from-gray-700 to-gray-900 hover:from-gray-900 hover:to-gray-700 text-white font-semibold rounded-lg shadow-lg transition duration-300 ease-in-out">Python</button>
+              <button className="px-6 py-3  bg-gradient-to-r from-gray-700 to-gray-900 hover:from-gray-900 hover:to-gray-700 text-white font-semibold rounded-lg shadow-lg transition duration-300 ease-in-out">JavaScript</button>
+              <button className="px-6 py-3  bg-gradient-to-r from-gray-700 to-gray-900 hover:from-gray-900 hover:to-gray-700 text-white font-semibold rounded-lg shadow-lg transition duration-300 ease-in-out">C</button>
+              <button className="px-6 py-3  bg-gradient-to-r from-gray-700 to-gray-900 hover:from-gray-900 hover:to-gray-700 text-white font-semibold rounded-lg shadow-lg transition duration-300 ease-in-out ">C++</button>
+              <button className="px-6 py-3  bg-gradient-to-r from-gray-700 to-gray-900 hover:from-gray-900 hover:to-gray-700 text-white font-semibold rounded-lg shadow-lg transition duration-300 ease-in-out">React</button>
+              <button className="px-6 py-3  bg-gradient-to-r from-gray-700 to-gray-900 hover:from-gray-900 hover:to-gray-700 text-white font-semibold rounded-lg shadow-lg transition duration-300 ease-in-out ">Node.js</button>
+              <button className="px-6 py-3  bg-gradient-to-r from-gray-700 to-gray-900 hover:from-gray-900 hover:to-gray-700 text-white font-semibold rounded-lg shadow-lg transition duration-300 ease-in-out ">Java</button>
+              <button className="px-6 py-3  bg-gradient-to-r from-gray-700 to-gray-900 hover:from-gray-900 hover:to-gray-700 text-white font-semibold rounded-lg shadow-lg transition duration-300 ease-in-out ">MongoDB</button>
+              <button className="px-6 py-3  bg-gradient-to-r from-gray-700 to-gray-900 hover:from-gray-900 hover:to-gray-700 text-white font-semibold rounded-lg shadow-lg transition duration-300 ease-in-out ">Git and Github</button>
+              <button className="px-6 py-3  bg-gradient-to-r from-gray-700 to-gray-900 hover:from-gray-900 hover:to-gray-700 text-white font-semibold rounded-lg shadow-lg transition duration-300 ease-in-out ">Data Structure and Algorithm</button>
+
+            </div>
+          </div>
+         
+        </div>
+      </div>
+      <Footer/>
     </div>
   );
 };
