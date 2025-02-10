d966651b35ed57e56ea32aa3f468512f0c05a0a2
divyanshgoyal777 <tonisha24680@gmail.com>
2025-02-10 16:37:12 +0530

    update

--- frontend/src/components/admin/hackathon/HackathonUpload.jsx
+++ frontend/src/components/admin/hackathon/HackathonUpload.jsx
@@ -43,10 +43,10 @@ const HackathonUpload = () => {
     if (!hackathonData.registerByDate)
       newErrors.registerByDate = "Registration deadline is required.";
     if (
-      new Date(hackathonData.registerByDate) > new Date(hackathonData.startDate)
+      new Date(hackathonData.registerByDate) >= new Date(hackathonData.endDate)
     )
       newErrors.registerByDate =
-        "Registration deadline must be before the start date.";
+        "Registration deadline must be before the end date.";
     if (hackathonData.categories.length === 0)
       newErrors.categories = "At least one category is required.";
     if (!hackathonData.registrationLink)
